d09444d41045fad2c197ab519a8277017ea4900d
Ernest D. Peterson <ernest@ernestpeterson.com>
2019-11-28 09:50:30 -0600

    Started the Facebook app section.

--- javascriptSection/js/main.js
+++ javascriptSection/js/main.js
@@ -1,3 +1,67 @@
 /*eslint-env jquery*/
 
+// global variables
+var myArray = [
+    "apple",
+    "orange",
+    "pear",
+    "pineaple",
+    "grape",
+    "pomegranite"
+];
+
+var myObject = {
+    "firstName": "Ernest",
+    "lastName": "Peterson",
+    "age": 49,
+    "hopes": [
+        "Mairage to a decent woman",
+        "become a web developer",
+        "have a famaly"
+    ],
+    "carreer": "Truck Driver"
+};
+
+// Facebook exercise section
+var database = [
+    {
+        "userName": "Ernest",
+        "password": "somePassword"
+    } 
+];
+
+var newsfeed = [
+    {
+        "userName": "",
+        "timeline": ""
+    },
+    {
+        "userName": "",
+        "timeline": ""
+    },
+    {
+        "userName": "",
+        "timeline": ""
+    }
+];
+
+// css for fun
 $("h1").css("text-align", "center");
+
+$("#firstSectionDiv1").css("height", "40vh");
+$("#firstSectionDiv1").css("background", "lightblue");
+
+$("#firstSectionDiv2").css("height", "40vh");
+$("#firstSectionDiv2").css("background", "gray");
+
+// the prompt function!
+$("#addEm").click(numTotal);
+
+function numTotal() {
+    let first = prompt("firstNum?");
+    let second = prompt("secondNum?");
+    $("#firstSectionDiv1").html(`${first} + ${second}`);
+    $("#firstSectionDiv2").html(Number(first) + Number(second));
+}
+
+console.log(myArray, myObject, database, newsfeed);
